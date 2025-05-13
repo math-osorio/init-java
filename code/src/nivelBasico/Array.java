785e9bb4c3cc2eb9786ad2051fc0cb6f9ca42c60
Matheus Henrique Gama Osório <henrique.busn@gmail.com>
2025-05-13 15:53:34 -0400

    ternarios adicionados

---
+++ code/src/nivelBasico/Array.java
@@ -0,0 +1,23 @@
+package nivelBasico;
+
+public class Array {
+    public static void main(String[] args){
+
+        String[] ninja = new String[3]; //array é um tipo de dado não primitivo
+
+
+
+
+
+
+
+        /* String ninja1 = "Naruto";
+        String ninja2 = "Sasuke";
+        String ninja3 = "Sakura";
+        System.out.println(ninja1);
+        System.out.println(ninja2);
+        System.out.println(ninja3); */
+
+
+    }
+}
