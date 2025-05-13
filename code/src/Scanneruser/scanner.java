08a6129c6782c2e81d287a71af4d67c7fb461ebf
biblioteca.sede <matheushenriquegama.mh@gmail.com>
2025-05-12 21:24:03 -0400

    adicionado scanner corretamente com as condições certas para verificar se o ninja é de maior ou de menor

--- code/src/Scanneruser/scanner.java
+++ code/src/Scanneruser/scanner.java
@@ -9,8 +9,25 @@ public class scanner {
         Scanner caixaTexto = new Scanner(System.in); // abrindo caixa de texto Scanner
 
         String name = caixaTexto.nextLine(); // nextLine() para armazenar uma String
-        System.out.println("O nome do ninja é: " + name);
+        System.out.println("Olá " + name + " !!");
 
-        caixaTexto.close(); // Fechar sempre o scanner com scanner.close
+         // Fechar sempre o scanner com scanner.close
+
+        System.out.println("-----------------------------------------------------");
+
+        System.out.println("Escreva a idade do ninja: ");
+        int idadeNinja = caixaTexto.nextInt();
+        System.out.println("A idade do Ninja é: " + idadeNinja);
+
+        if(idadeNinja > 18){
+            System.out.println("O ninja está pronto para sair na missão");
+        } else {
+            System.out.println("O ninja ainda é menor de idade e não pode sair numa missão!");
+        }
+
+        caixaTexto.close();
+
+
+
     }
 }
