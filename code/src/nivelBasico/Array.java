0bbb3093309659852a10e913215f53611e1bbad5
Matheus Henrique Gama Osório <henrique.busn@gmail.com>
2025-05-14 16:14:33 -0400

    arrays adicionados

--- code/src/nivelBasico/Array.java
+++ code/src/nivelBasico/Array.java
@@ -3,7 +3,17 @@ package nivelBasico;
 public class Array {
     public static void main(String[] args){
 
-        String[] ninja = new String[3]; //array é um tipo de dado não primitivo
+        String[] ninja = new String[4]; //array é um tipo de dado não primitivo
+        ninja[0] = "Naruto Uzumaki"; // every programming element starts at 0
+        ninja[1] = "Sasuke Uchiha";
+        ninja[2] = "Sakura Haruno";
+        ninja[3] = "Hinata Hyuga";
+        /*
+        * Preciso liberar mais espaço na memória se quiser
+        * adicionar mais elementos no meu array !!*/
+
+        System.out.println(ninja[3]);
+
 
 
 
